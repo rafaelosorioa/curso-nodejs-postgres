1f94f2ca8e2a3e89225cf1db93fad2c40be47c04
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2024-01-04 16:21:06 -0500

    Class-8: Add connection using environment

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,14 +1,12 @@
 const { Pool } = require('pg');
+const config = require('../config/config');
 
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 //Create pool connection to the database
-//TODO: use enviroment variables
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'rafa',
-  password: 'admin123',
-  database: 'my_store',
-});
+
+const pool = new Pool({ connectionString: URI });
 //Only need to pass the pool to be instancied in the service no need to create a connection
 //or return it
 module.exports = pool;
