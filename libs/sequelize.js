ea3fc3ca1fa426e718792bc76c463091f0122c13
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2024-01-05 18:23:31 -0500

    Class-12: Change DB to mysql

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,10 +4,10 @@ const setupModels = require('../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   // eslint-disable-next-line no-console
   logging: console.log,
 });
