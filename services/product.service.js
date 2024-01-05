1de594c166aeee8764130d21804951a19da9a10a
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2024-01-05 09:31:02 -0500

    Class-8: Use Sequelize in the service

--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    //In case of error in the pool
-    this.pool.on('error', (err) => console.log(err));
   }
 
   generate() {
@@ -36,8 +33,8 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const response = await this.pool.query(query);
-    return response.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
