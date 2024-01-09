8fb609a6d0d2309b928f8c1d8c9ea310d6981a71
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2024-01-09 17:26:55 -0500

    Class-16: Add customer model

--- db/models/index.js
+++ db/models/index.js
@@ -1,12 +1,17 @@
 const { User, UserSchema } = require('./user.model');
+const { Customer, CustomerSchema } = require('./customer.model');
 const { Product, ProductSchema } = require('./product.model');
 const { Category, CategorySchema } = require('./category.model');
 
 //Add our schemas
 function setupModels(sequelize) {
   Category.init(CategorySchema, Category.config(sequelize));
-  User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  User.init(UserSchema, User.config(sequelize));
+
+  //Relations
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
