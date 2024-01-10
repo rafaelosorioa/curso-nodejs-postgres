abafb4d976d70a68410ddc45a1736449389b0f04
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2024-01-10 14:28:35 -0500

    Class-19: Create migration
    Migrate the tables for products and categories, we must follow the order without the category table cannot create the product for the relation

--- db/migrations/20240110131610-create-products-categories.js
+++ db/migrations/20240110131610-create-products-categories.js
@@ -5,16 +5,16 @@ const {
   CategorySchema,
   CATEGORY_TABLE,
 } = require('./../models/category.model');
-const { ProductModel, PRODUCT_TABLE } = require('./../models/product.model');
+const { PRODUCT_TABLE, ProductSchema } = require('./../models/product.model');
 
 module.exports = {
   async up(queryInterface) {
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
-    await queryInterface.createTable(PRODUCT_TABLE, ProductModel);
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
   },
 };
